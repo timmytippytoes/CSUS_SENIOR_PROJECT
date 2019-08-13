490eb65c4af4e36d6a4af9ec23749efa983b4faa
unknown <timothydarrow@csus.edu>
2019-08-13 00:13:52 -0700

    Added menu bar and carousel

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function(){
+
+	"use strict";
+	$('.slider-carousel').carouFredSel({
+		responsive: true,
+		width: '100%',
+		circular: true,
+		scroll:{
+			items:1,
+			duration: 500,
+			pauseOnHover: true
+		},
+		auto: false,
+		prev: {
+			button: "#button_prev",
+			key: "left"
+		},
+		next: {
+			button: "#button_next",
+			key: "right"
+		},
+		items:
+		{
+			visible:{
+				min:1,
+				max:1
+			},
+			height:"variable"
+		},
+		pagination:{
+			container: ".sliderpager",
+			pageAnchorBuilder: false
+		}
+	});
+	
+});
